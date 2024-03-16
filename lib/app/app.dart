debdf44f77da7e01ca1735646672a4f228fe000e
Javier Gomez <javiergomezve@gmail.com>
2024-03-16 10:36:32 -0500

    Refactor files & folders structure

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/app/view/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'ToDo App'),
+    );
+  }
+}
